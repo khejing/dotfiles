3ca2aaccaaa23f086b14f4cd4c57dc2d623b7ce2
Will Lee <lizhi@wedocare.com>
2019-02-20 10:55:59 +0800

    oh my zsh自定义启动脚本上传

---
+++ example.zsh
@@ -0,0 +1,11 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+alias gh='git log --pretty=format:"%C(auto)%h %ad | %C(auto)%s%d  %Cblue(%an)" --graph --date=short'
