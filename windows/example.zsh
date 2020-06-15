4a1749907455214f5f58d7a2fee77b5737fb4eee
Will Lee <khejing3@aliyun.com>
2020-06-15 08:34:38 +0800

    把vim中不靠谱的ZZ快捷键和不应该保存的.zshrc都删了，并且把给wsl用的example.zsh上库了

---
+++ windows/example.zsh
@@ -0,0 +1,15 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+alias gh='git log --pretty=format:"%C(auto)%h %ad | %C(auto)%s%d" --date=format:"%y-%m-%d %R"'
+alias ggr='git grep -i'
+alias grhl='git reset HEAD^'
+alias myproxy='export ALL_PROXY=socks5://172.25.240.1:10808'
+alias unproxy='unset ALL_PROXY'
