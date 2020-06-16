fd1efa555a36756970b8a7a1fdd2cb8519422240
khejing <khejing@hotmail.com>
2020-06-16 23:22:16 +0800

    git的ssh上库也要支持走代理

--- windows/example.zsh
+++ windows/example.zsh
@@ -8,8 +8,11 @@
 # brainstormr=~/Projects/development/planetargon/brainstormr
 # cd $brainstormr
 #
+myproxy() {
+  export ALL_PROXY='socks5://172.25.240.1:10808'
+  git config --global core.sshCommand 'ssh -o "ProxyCommand=nc -x 172.25.240.1:10808 %h %p"'
+}
 alias gh='git log --pretty=format:"%C(auto)%h %ad | %C(auto)%s%d" --date=format:"%y-%m-%d %R"'
 alias ggr='git grep -i'
 alias grhl='git reset HEAD^'
-alias myproxy='export ALL_PROXY=socks5://172.25.240.1:10808'
-alias unproxy='unset ALL_PROXY'
+alias unproxy='unset ALL_PROXY;git config --global --unset core.sshCommand'
