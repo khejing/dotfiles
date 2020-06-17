c2dbd26068295e1c59166809adb2c124c235ff4e
khejing <khejing@hotmail.com>
2020-06-17 16:56:59 +0800

    对wsl来说的windows ip是会变的，所以每次设置代理前都要读取一下才行

--- windows/example.zsh
+++ windows/example.zsh
@@ -8,9 +8,10 @@
 # brainstormr=~/Projects/development/planetargon/brainstormr
 # cd $brainstormr
 #
+windowsIP=`awk '/nameserver/ {print $2}' /etc/resolv.conf`
 myproxy() {
-  export ALL_PROXY='socks5://172.25.240.1:10808'
-  git config --global core.sshCommand 'ssh -o "ProxyCommand=nc -x 172.25.240.1:10808 %h %p"'
+  export ALL_PROXY="socks5://$windowsIP:10808"
+  git config --global core.sshCommand "ssh -o \"ProxyCommand=nc -x $windowsIP:10808 %h %p\""
 }
 alias gh='git log --pretty=format:"%C(auto)%h %ad | %C(auto)%s%d" --date=format:"%y-%m-%d %R"'
 alias ggr='git grep -i'
