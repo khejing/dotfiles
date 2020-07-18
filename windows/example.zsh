8489380ef4baa81667ffb04a6421dee283fe1f3c
Will Lee <khejing3@aliyun.com>
2020-07-18 11:49:56 +0800

    为了使用代理修改git配置是不对的，应修改环境变量

--- windows/example.zsh
+++ windows/example.zsh
@@ -11,9 +11,9 @@
 windowsIP=`awk '/nameserver/ {print $2}' /etc/resolv.conf`
 myproxy() {
   export ALL_PROXY="socks5://$windowsIP:10808"
-  git config --global core.sshCommand "ssh -o \"ProxyCommand=nc -x $windowsIP:10808 %h %p\""
+  export GIT_SSH_COMMAND="ssh -o \"ProxyCommand=nc -x $windowsIP:10808 %h %p\""
 }
+alias unproxy='unset ALL_PROXY;unset GIT_SSH_COMMAND'
 alias gh='git log --pretty=format:"%C(auto)%h %ad | %C(auto)%s%d" --date=format:"%y-%m-%d %R"'
 alias ggr='git grep -i'
 alias grhl='git reset HEAD^'
-alias unproxy='unset ALL_PROXY;git config --global --unset core.sshCommand'
